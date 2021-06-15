2d5f2c2c077c7b267a62404022692d31e03bb6a5
Christine Ibaraki <christine.ibaraki@gmail.com>
2021-06-15 13:22:42 -0400

    notebooks to run baseline models & compute monthly data

--- core/aggregate_daily_data.py
+++ core/aggregate_daily_data.py
@@ -1,11 +1,10 @@
 """ Functions to aggregate daily assessment data to monthly counts"""
 
-import datetime as dt
 import pandas as pd
 pd.options.mode.chained_assignment = None
 
 
-def clean_rows(df):
+def clean_rows_helper(df):
     """
     Remove na kick-off dates, remove duplicates
     :param df: pandas dataframe containing daily assessments
@@ -17,7 +16,7 @@ def clean_rows(df):
     return df
 
 
-def create_date_columns(df):
+def create_date_columns_helper(df):
     """
     Create date columns (based on assessment kick off date)
     :param df: pandas dataframe containing daily assessments
@@ -33,7 +32,7 @@ def create_date_columns(df):
     return df
 
 
-def one_hot_encode_assessment(df):
+def one_hot_encode_assessment_helper(df):
     """
     One hot encode assessment types, then sum cols (should be =1)
     Join back to daily df
@@ -47,7 +46,7 @@ def one_hot_encode_assessment(df):
     return df
 
 
-def subset_data_by_schedule_status(df):
+def subset_data_by_schedule_status_helper(df):
     """
     Subset to only rows where status is one of:
         Completed
@@ -64,40 +63,26 @@ def subset_data_by_schedule_status(df):
     return df
 
 
-def create_monthly_aggregate_from_daily_data(df):
+def create_monthly_aggregate_from_daily_data_helper(df):
     """
-    Compute monthly assessment counts
+    Compute monthly assessment counts helper
     :param df: pandas dataframe containing daily assessments
     :return: dataframe where each row represents month
     """
     daily_restricted = df[['Date', 'ATT','LSCA','OA','SCA','TRA','total_assessments_computed']]
     monthly_data = daily_restricted.groupby('Date').sum()
     return monthly_data
-#
-#
-#
-#
-# import os
-# import datetime as dt
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import matplotlib.dates as mdates
-# import seaborn as sns
-#
-# df_daily = pd.read_csv(os.path.join('.', 'Assessment Data_Daily.csv'))
-# print(f"shape daily data: {df_daily.shape}")
-# df_daily = clean_rows(df_daily)
-# print(f"shape daily data: {df_daily.shape}")
-# df_daily = create_date_columns(df_daily)
-# print(f"shape daily data: {df_daily.shape}")
-# df_daily = one_hot_encode_assessment(df_daily)
-# print(f"shape daily data: {df_daily.shape}")
-# df_daily = subset_data_by_schedule_status(df_daily)
-# print(f"shape daily data: {df_daily.shape}")
-#
-# df_daily.head()
-#
-# df_monthly = create_monthly_aggregate_from_daily_data(df_daily)
-# df_monthly.tail(n=10)
-# len(df_monthly )
+
+
+def compute_monthly_assessment_counts(df):
+    """
+    Compute monthly assessment counts
+    :param df: unprocessed pandas dataframe containing daily assessments
+    :return: dataframe of monthly assessment ct data
+    """
+
+    df = clean_rows_helper(df)
+    df = create_date_columns_helper(df)
+    df = one_hot_encode_assessment_helper(df)
+    df = subset_data_by_schedule_status_helper(df)
+    return create_monthly_aggregate_from_daily_data_helper(df)
